554e7291789df9ef0f82d0c5f404356714ea2cef
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-03-06 14:56:18 +0700

    complted 100%

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <div className="col-span-2 w-full h-full hidden sm:block">
           <SidebarComponent/>
         </div>
-        <div className="col-span-10 bg-slate-100 w-full h-full">
+        <div className="col-span-10 w-full h-full overflow-y-scroll">
           <div className="p-4">
             <TopNavbarComponent  onSearch={handleSearchQuery}/>
           </div>
